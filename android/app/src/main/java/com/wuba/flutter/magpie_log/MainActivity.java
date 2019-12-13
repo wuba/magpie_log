8dcb28a549f708ec2bbdc880c28317d5463f6489
linyueyang <linyueyang@58.com>
2019-12-13 15:31:06 +0800

    init

---
+++ android/app/src/main/java/com/wuba/flutter/magpie_log/MainActivity.java
@@ -0,0 +1,13 @@
+package com.wuba.flutter.magpie_log;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
