24b263af0dd69aabfc70d0f925f8835a2834861d
linyueyang <linyueyang@58.com>
2020-06-12 10:22:48 +0800

    1.解决flutterBoost等混合开发框架兼容问题，包括监听，pageName兼容 2.解决页面Context统一获取的问题，用route替换 3.解决数据解析级联问题和一些异常的处理 4.demo的完善，注释的完善，文档的完善

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,47 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<device_info/FLTDeviceInfoPlugin.h>)
+#import <device_info/FLTDeviceInfoPlugin.h>
+#else
+@import device_info;
+#endif
+
+#if __has_include(<flutter_boost/FlutterBoostPlugin.h>)
+#import <flutter_boost/FlutterBoostPlugin.h>
+#else
+@import flutter_boost;
+#endif
+
+#if __has_include(<fluttertoast/FluttertoastPlugin.h>)
+#import <fluttertoast/FluttertoastPlugin.h>
+#else
+@import fluttertoast;
+#endif
+
+#if __has_include(<magpie_log/MagpieLogPlugin.h>)
+#import <magpie_log/MagpieLogPlugin.h>
+#else
+@import magpie_log;
+#endif
+
+#if __has_include(<path_provider/FLTPathProviderPlugin.h>)
+#import <path_provider/FLTPathProviderPlugin.h>
+#else
+@import path_provider;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTDeviceInfoPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTDeviceInfoPlugin"]];
+  [FlutterBoostPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterBoostPlugin"]];
+  [FluttertoastPlugin registerWithRegistrar:[registry registrarForPlugin:@"FluttertoastPlugin"]];
+  [MagpieLogPlugin registerWithRegistrar:[registry registrarForPlugin:@"MagpieLogPlugin"]];
+  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
+}
+
+@end
